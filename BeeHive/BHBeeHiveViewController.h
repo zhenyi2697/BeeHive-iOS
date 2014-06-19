3f91390630b1d6d5f730e371d6770c8897ba5bf4
C <fusaix.c@gmail.com>
2014-06-19 17:44:33 -0400

    Ready for BeeHive main view
    we can’t put BeeHive as 1st tab yet

---
+++ BeeHive/BHBeeHiveViewController.h
@@ -0,0 +1,13 @@
+//
+//  BHBeeHiveViewController.h
+//  BeeHive
+//
+//  Created by Louis CHEN on 6/19/14.
+//  Copyright (c) 2014 Zhenyi Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BHBeeHiveViewController : UIViewController
+
+@end
